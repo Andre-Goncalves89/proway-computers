ef5b5ade2c66ef4d8814eb200813161e3e38910b
Andre Goncalves <andrearaujomti@gmail.com>
2026-07-28 00:00:30 -0300

    fix-arquivo routes corrigido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+  {
+    path: 'produtos',
+    loadComponent: () => import('./produtos/produtos').then(m => m.ProdutosComponent)
+  }
+];
