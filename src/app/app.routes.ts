cac723db1be9b67aca07fd854c11d92342fea3f0
Andre Goncalves <andrearaujomti@gmail.com>
2026-07-29 17:43:48 -0300

    feat: adiciona pagina 404 nao encontrada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { NaoEncontrado } from './nao-encontrado/nao-encontrado';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'produtos', pathMatch: 'full' },
   {
     path: 'produtos',
     loadComponent: () => import('./produtos/produtos').then(m => m.ProdutosComponent)
-  }
+  },
+  {path: '**', component: NaoEncontrado}
 ];
